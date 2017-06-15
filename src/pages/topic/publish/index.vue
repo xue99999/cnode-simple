b8d077cf4d03cf7b34246f1e7f7da0995d01ba1d
allmylife <1090374167@qq.com>
2017-06-15 23:35:50 +0800

    publish page is add and methods is no writing

--- src/pages/topic/publish/index.vue
+++ src/pages/topic/publish/index.vue
@@ -6,14 +6,45 @@
       </div>
     </v-header>
     <v-content>
-    2432143224
-    <v-loading></v-loading>
+        <form class="form" @submit.prevent="submit">
+          <div class="title">
+            <input type="text" placeholder="标题..." v-model="form.title">
+          </div>
+          <div class="select">
+            <select v-model="form.tab">
+              <option value="">请选择</option>
+              <option value="share">分享</option>
+              <option value="ask">问答</option>
+              <option value="job">招聘</option>
+            </select>
+          </div>
+          <div class="con">
+            <textarea name="" id="" cols="30" rows="10" placeholder="内容..." v-model="form.content"></textarea>
+          </div>
+        </form>
     </v-content>
 		<v-footer></v-footer>
 	</div>
 </template>
 
+<script>
+  export default {
+    data() {
+      return {
+        form: {
+          title: '',
+          tab: '',
+          content: ''
+        }
+      }
+    }
+  }
+</script>
+
 <style lang="less">
+  * {
+    outline: none;
+  }
 	.publish_wrap {
       .item {
         .icon-edit {
@@ -23,6 +54,36 @@
         &:active {
           background-color: rgba(0, 0, 0, .1);
         }
+      }
+      .form {
+          .title {
+            padding: 5px 15px;
+            input {
+              box-sizing: border-box;
+              width: 100%;
+              padding: 0 5px;
+              font-size: 14px;
+              line-height: 30px;
+            }
+          }
+          .select {
+            box-sizing: border-box;
+            border-top: 1px solid #ddd;
+            border-bottom: 1px solid #ddd;
+            select {
+              padding: 10px 5px;
+              width: 100%;
+            }
+          }
+          .con {
+            padding: 5px 10px;
+            textarea {
+              border: none;
+              width: 100%;
+              padding: 0 5px;
+              resize: none;
+            }
+          }
       }
       
   }
